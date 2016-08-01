643adbc63e316e3571a1069b4f2e3b4446aec0c6
David Yang <daworld.ny@gmail.com>
2016-08-01 22:28:14 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'route'=> 'MailForm:Admin:Form@gridAction',
-        'icon' => 'fa fa-envelope fa-5x',
-        'name' => 'Mail forms',
-        'order' => 1,
-        'description' => 'Mail forms module allows you to manager forms that send data from your site to your email',
-    )
+ 'route'=> 'MailForm:Admin:Form@gridAction',
+ 'icon' => 'fa fa-envelope fa-5x',
+ 'name' => 'Mail forms',
+ 'order' => 1,
+ 'description' => 'Mail forms module allows you to manager forms that send data from your site to your email',
 );
