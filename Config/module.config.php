17502191bdcb2b011f33d1803fff7974bc602095
David Yang <daworld.ny@gmail.com>
2016-08-02 21:56:49 +0500

    Tweak for module manager

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,8 @@
 return array(
  'route'=> 'MailForm:Admin:Form@gridAction',
  'icon' => 'fa fa-envelope fa-5x',
- 'name' => 'Mail forms',
+ 'name' => 'MailForm',
+ 'caption' => 'Mail forms',
  'order' => 1,
  'description' => 'Mail forms module allows you to manager forms that send data from your site to your email',
 );
