622c412b8c32cd4696f248e10d866b42a9d59814
dave-yang <daworld.ny@gmail.com>
2018-11-25 15:47:26 +0500

    FieldService implementation

--- Service/FieldService.php
+++ Service/FieldService.php
@@ -11,6 +11,7 @@
 
 namespace MailForm\Service;
 
+use Krystal\Stdlib\VirtualEntity;
 use Cms\Service\AbstractManager;
 use MailForm\Storage\FieldMapperInterface;
 
@@ -32,5 +33,79 @@ final class FieldService extends AbstractManager
     public function __construct(FieldMapperInterface $fieldMapper)
     {
         $this->fieldMapper = $fieldMapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function toEntity(array $row)
+    {
+        $entity = new VirtualEntity();
+        $entity->setId($row['id'])
+               ->setLangId($row['lang_id'])
+               ->setFormId($row['form_id'])
+               ->setType($row['type'])
+               ->setName($row['name']);
+
+        return $entity;
+    }
+
+    /**
+     * Fetch field by ID
+     * 
+     * @param int $id Field ID
+     * @param boolean $withTranslations Whether to fetch translations or not
+     * @return array
+     */
+    public function fetchById($id, $withTranslations)
+    {
+        if ($withTranslations == true) {
+            return $this->prepareResults($this->fieldMapper->fetchById($id, true));
+        } else {
+            return $this->prepareResult($this->fieldMapper->fetchById($id, false));
+        }
+    }
+
+    /**
+     * Fetch all fields by associated form ID
+     * 
+     * @param int $formId
+     * @return array
+     */
+    public function fetchAll($formId)
+    {
+        return $this->prepareResults($this->fieldMapper->fetchAll($formId));
+    }
+
+    /**
+     * Returns last ID
+     * 
+     * @return int
+     */
+    public function getLastId()
+    {
+        return $this->fieldMapper->getMaxId();
+    }
+
+    /**
+     * Saves a field
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function save(array $input)
+    {
+        return $this->fieldMapper->saveEntity($input['field'], $input['translation']);
+    }
+
+    /**
+     * Deletes a field by its ID
+     * 
+     * @param int $id Field ID
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->fieldMapper->deleteEntity($id);
     }
 }
